c443bb6c80d6231b4506c1cc8a2e93de55e67e15
Aida Khubaeva <aidakhubaeva@yandex.ru>
2024-02-01 17:30:02 +0100

    second commit

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,50 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+document.addEventListener('DOMContentLoaded', () => {
+  const placesList = document.querySelector('.places__list');
+  const cardTemplate = document.querySelector('#card-template').content;
+  const addCardButton = document.querySelector('.profile__add-button');
+
+  const imageUrls = [
+      './images/card_1.jpg',
+      './images/card_2.jpg',
+      './images/card_3.jpg',
+      './images/card_4.jpg',
+      './images/card_5.jpg',
+      './images/card_6.jpg'
+  ];
+
+  const cardTitles = [
+    'Вдохновляйся',
+    'Наслаждайся',
+    'Путешествуй',
+    'Преодолевай',
+    'Влюбляйся',
+    'Познавай',
+  ]
+
+  let currentImageIndex = 0; 
+
+
+  function createCard() {
+      const cardElement = cardTemplate.cloneNode(true);
+      cardElement.querySelector('.card__image').src = imageUrls[currentImageIndex];
+      cardElement.querySelector('.card__image').alt = 'Изображение ' + (currentImageIndex + 1);
+      cardElement.querySelector('.card__title').textContent = cardTitles[currentImageIndex];
+
+      cardElement.querySelector('.card__delete-button').addEventListener('click', function () {
+          this.closest('.places__item').remove();
+      });
+
+      currentImageIndex = (currentImageIndex + 1) % imageUrls.length;
+
+      return cardElement;
+  }
+
+  addCardButton.addEventListener('click', () => {
+      const cardElement = createCard();
+      placesList.appendChild(cardElement);
+  });
+});
