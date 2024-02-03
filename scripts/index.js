46fb4c229395d786d878fa184e98f8cd43d80125
Aida Khubaeva <aidakhubaeva@yandex.ru>
2024-02-03 19:12:34 +0100

    fourth commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,20 @@
-// @todo: Темплейт карточки
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+function closest() {
+    this.closest('.places__item').remove();
+}
 
-// @todo: Функция создания карточки
+function createCard(text, image) {
+    const cardElement = cardTemplate.cloneNode(true);
+    cardElement.querySelector('.card__image').src = image;
+    cardElement.querySelector('.card__image').alt = text;
+    cardElement.querySelector('.card__title').textContent = text;
+    cardElement.querySelector('.card__delete-button').addEventListener('click', closest);
+    return cardElement;
+}
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
-
-document.addEventListener('DOMContentLoaded', () => {
-    const placesList = document.querySelector('.places__list');
-    const cardTemplate = document.querySelector('#card-template').content;
-    const addCardButton = document.querySelector('.profile__add-button');
-  
-    let currentImageIndex = 0; 
-  
-    function createCard() {
-        const cardElement = cardTemplate.cloneNode(true);
-        cardElement.querySelector('.card__image').src = initialCards[currentImageIndex].image;
-        cardElement.querySelector('.card__image').alt = initialCards[currentImageIndex];
-        cardElement.querySelector('.card__title').textContent = initialCards[currentImageIndex].text;
-  
-        cardElement.querySelector('.card__delete-button').addEventListener('click', (closest));
-
-        function closest() {
-            this.closest('.places__item').remove();
-        }
-        currentImageIndex = (currentImageIndex + 1) % initialCards.length;
-  
-        return cardElement;
-    }
-  
-    addCardButton.addEventListener('click', () => {
-        const cardElement = createCard();
-        placesList.appendChild(cardElement);
-    });
-  });
-  
+initialCards.forEach((cardData) => {
+    const cardElement = createCard(cardData.text, cardData.image);
+    placesList.appendChild(cardElement);
+});
